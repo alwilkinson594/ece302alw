438278a237b4a282f1287bb6c6f3d3c0081580c6
Alexis Wilkinson <alw223@pitt.edu>
2021-01-21 12:28:12 -0500

    added hw00

---
+++ homework/hw00/add/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexis/Documents/ece302/homework/hw0/add
+# Build directory: /home/alexis/Documents/ece302/homework/hw0/add
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "add-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/alexis/Documents/ece302/homework/hw0/add/CMakeLists.txt;16;add_test;/home/alexis/Documents/ece302/homework/hw0/add/CMakeLists.txt;0;")
--- homework/hw00/add/add_tests.cpp
+++ homework/hw00/add/add_tests.cpp
@@ -10,4 +10,16 @@ TEST_CASE("Test add", "[Add]"){
 
   Add<int> a;
   REQUIRE(a.call(1,2)==3);
+
+  Add<int> b;
+  REQUIRE(b.call(5,7)==12);
+
+  Add<int> c;
+  REQUIRE(c.call(13,21)==34);
+
+  Add<int> d;
+  REQUIRE(d.call(22,53)==75);
+
+  Add<int> e;
+  REQUIRE(e.call(4,6)==10);
 }
